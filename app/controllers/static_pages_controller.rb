bf09bb5895c556f8ec713f2acd84e191606dd153
Michael Welsh <brohemian1989@gmail.com>
2017-01-16 17:30:42 -0500

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  	parameters = { term: params[:term], limit: 16 }
+
+    # render json: Yelp.client.search(‘San Francisco’, parameters)
+    @response = Yelp.client.search('Philadelphia')
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+
+end
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,10 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.token = ENV['TOKEN']
+  config.token_secret = ENV['TOKEN_SECRET']
+end
+
+Yelp.client.search('San Francisco', { term: 'food' })
