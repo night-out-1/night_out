e69e74573e473725e2fe61d67066697df9f03b28
Michael Welsh <brohemian1989@gmail.com>
2017-01-17 11:14:37 -0500

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170117153726) do
+ActiveRecord::Schema.define(version: 20170117154920) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -20,8 +19,7 @@ ActiveRecord::Schema.define(version: 20170117153726) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-ActiveRecord::Schema.define(version: 20170117154920) do
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -53,7 +51,6 @@ ActiveRecord::Schema.define(version: 20170117154920) do
     t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
