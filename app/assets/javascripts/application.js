844817c59a5779483a359ba01509315821b118e6
Michael Welsh <brohemian1989@gmail.com>
2017-01-19 15:21:43 -0500

    Started heavy styling. Did too much to commmit each one. Need to get control of my life

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,32 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+$(document).ready(function(){
+    $('.jumbotron').hide();
+    $('.jumbotron').fadeIn(6000);
+
+
+    $(function() {
+    $(window).scroll( function(){
+    
+       
+        $('.fadeIn').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+           
+            bottom_of_window = bottom_of_window + 200;  
+          
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},4000);
+                    
+            }
+        }); 
+    
+    });
+});
+
+});
