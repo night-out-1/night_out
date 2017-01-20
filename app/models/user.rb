6e58658cbb6cc47abc33b3523589d42dc5cee239
Mia Adjei <adjei.m@gmail.com>
2017-01-20 12:13:55 -0500

    Small missing.png image now displays for users who did not upload an avatar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 	has_and_belongs_to_many :events
   has_many :comments
 
-	has_attached_file :avatar, styles: { medium: "300x300>", small: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", small: "100x100>" }, default_url: "/avatars/:style/missing.png"
 	
 	validates_attachment_content_type :avatar,
         :content_type => /\Aimage\/.*\Z/
