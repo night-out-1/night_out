1b10ee8d726caa0ed10366a5689b7cce057b2509
Brynne Zdybel <bazdybel@gmail.com>
2017-01-21 15:34:40 -0500

    added logic to profile index controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,15 @@ class ProfilesController < ApplicationController
 
   def index
   	@users = User.all
+  	if current_user
+    	@user = User.find(current_user.id)
+    	@events = Event.all
+    	@array = []
+    	@events.each do |event|
+      		array = [event.id, event.location_latitude.to_f, event.location_longitude.to_f]
+      		@array.push(array)
+   		end
+    end
   end
 
   def show
