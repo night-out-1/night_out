23763c007739b270e03136ca6cc5fc18ae0e7859
Brynne Zdybel <bazdybel@gmail.com>
2017-01-21 22:54:02 -0500

    fixed a few JS error bugs and added comments for my sanity

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,18 +48,23 @@ $(document).ready(function(){
 
 window.onload = getLocation
 
+//these variables are set to determine when certain functions will run. 
+//they are changed both in html script tags and in this JS file. bz
 var longitude = null;
 var latitude = null;
-var on_events = true;
+var enabled = false;
 
+//this function finds the location of the users device and calls showPosition and showError. bz
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        console.log("Geolocation is not supported by this browser.  This console log is coming from the getLocation function");
     }
 }
 
+//this function sets latitude and longitude to the latitude and longitude of the user's device. 
+//console logging for presentation purposes. bz
 function showPosition(position) {
 	latitude = position.coords.latitude
 	longitude = position.coords.longitude
@@ -67,12 +72,34 @@ function showPosition(position) {
  	console.log("Aye, aye, sir.");
 }
 
+//this function will occur when location services are disabled on the device. 
+//console log for clarity and also shows zip_events since geolocated events will not show. bz
 function showError(error) {
 	console.log("Dammit, Jim, I'm a function, not a gps!");
-	console.log("Bones, you could have just told me geolocation was turned off.")
-	document.getElementById("zip_events").classList.remove("hidden");
+	console.log("Bones, you could have just told me geolocation was disabled.")
+	if (enabled==true){
+		console.log("Geordi, enable the search bar.");
+		console.log("Sir, you will get an error.");
+		console.log("Does it effect functionality?");
+		console.log("No, sir.  But it could be fixed.");
+		console.log("We'll cross that bridge when it comes. Enable the search bar.");
+		console.log("Aye, aye,  captain.");
+		document.getElementById("zip_events").classList.remove("hidden");
+	};
 }
 
+//this function is called on the events page.
+//the if statement will run when the geolocation has succeeded (latitude = something). 
+//it finds the distance between the device (geolocated) and the event (saved in database) using the pythagorean theorem. 
+//please note that this method, though effective, due to the nature of longitude, is imperfect.
+//it then checks if the distance is less than .145, which for latitude is roughly ten miles (again, imperfect).
+//if the distance is less than .145, the id of the event is pushed into an array.
+//this array is then used to choose which event cards are shown on the event page by removing the class for display: hidden.
+//because this function is called whether or not the search bar is enabled, the else statement will run if it is not.
+//the else statement will show events based on zip code by removing their class for display:hidden.
+//the if enabled=true is present to prevent JS errors.
+//enabled is automatically set to false and is changed to true when the search bar is enabled.
+//please note that there is additional logic for this function's functions in the events#index view and the events controller. -bz
 function getLatLong(){
 	if (latitude != null){
 		var nearby_ids = [];
@@ -84,14 +111,16 @@ function getLatLong(){
 				nearby_ids.push(array[i][0]);
 			};
 		};
-		console.log("Captain, nearby ids has been set.");
+		console.log("Data, have the geolocated events been found?");
+		console.log("Affirmative, captian.");
 		for (var i=0; i<nearby_ids.length; i++){
 			document.getElementById(nearby_ids[i].toString()).classList.remove("hidden");
 			document.getElementById(nearby_ids[i].toString()).style.display= "block";
 		};
 	}
 	else{
-		console.log("location services off");
+		console.log("Captain, shall show the user-zip-code based events?");
+		console.log("Make it so, number one.")
 		document.getElementById("zip_events").classList.remove("hidden");
 	};
 
